984b26d80219ad171344a9e58e35897e98a83dd6
Edrenn <edern.rodriguez@orange.fr>
2017-06-13 07:19:59 +0200

    Ajout utilisation Potion

--- Clickers/ViewModel/ArmyFolder/HeroFightViewModel.cs
+++ Clickers/ViewModel/ArmyFolder/HeroFightViewModel.cs
@@ -24,18 +24,18 @@ namespace Clickers.ViewModel.Army
             set { view = value; }
         }
 
-        private Hero attackingHero;
-        public Hero AttackingHero
+        private Hero allyHero;
+        public Hero AllyHero
         {
-            get { return attackingHero; }
-            set { attackingHero = value; }
+            get { return allyHero; }
+            set { allyHero = value; }
         }
 
-        private Hero defendingHero;
-        public Hero DefendingHero
+        private Hero enemyHero;
+        public Hero EnemyHero
         {
-            get { return defendingHero; }
-            set { defendingHero = value; }
+            get { return enemyHero; }
+            set { enemyHero = value; }
         }
 
         private Clickers.Models.Army attackingArmy;
@@ -66,12 +66,12 @@ namespace Clickers.ViewModel.Army
         {
             this.View = new HeroDuelFightView();
             this.Actions = new Dictionary<Hero, string>();
-            this.AttackingHero = attackingArmy.Hero;
-            this.DefendingHero = defendingArmy.Hero;
+            this.AllyHero = attackingArmy.Hero;
+            this.EnemyHero = defendingArmy.Hero;
             this.attackingArmy = attackingArmy;
             this.defendingArmy = defendingArmy;
             this.attackedCastle = attackedCastle;
-            GenerateUI(AttackingHero, DefendingHero);
+            GenerateUI(AllyHero, EnemyHero);
             View.TurnTB.DataContext = this;
             Switcher.Switch(this.View);
         }
@@ -112,8 +112,8 @@ namespace Clickers.ViewModel.Army
 
             }
             this.View.AllyHeroSP.Children.Add(AllyHeroView);
-            this.View.AllyHeroAttributesSP.DataContext = this.AttackingHero;
-            this.View.EnnemyHeroAttributesSP.DataContext = this.DefendingHero;
+            this.View.AllyHeroAttributesSP.DataContext = this.AllyHero;
+            this.View.EnnemyHeroAttributesSP.DataContext = this.EnemyHero;
 
             HeroView EnnemyHeroView = new HeroView();
             EnnemyHeroView.HeroInfoSP.Visibility = System.Windows.Visibility.Collapsed;
@@ -121,56 +121,72 @@ namespace Clickers.ViewModel.Army
             EnnemyHeroView.DataContext = defendingHero;
             this.View.ValidateButton.Click += ValidateButton_Click;
             this.View.EnnemyHeroSP.Children.Add(EnnemyHeroView);
+            this.View.ItemsButton.Click += ItemsButton_Click;
+        }
+
+        private void ItemsButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.AllyHero.Potion != null)
+            {
+                DoAllActions("Objet", AITurn());
+            }
+            else
+                System.Windows.MessageBox.Show("Vous n'avez pas de potion dans votre inventaire");
         }
 
         private void ValidateButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            AITurn();
-            DoAllActions();
+            //AITurn();
+            //DoAllActions();
         }
 
         private void FeinteButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            if (AttackingHero.Skills[2].UseCounter == 0)
-            {
-                System.Windows.MessageBox.Show("Vous avez utilisé toutes vos feintes");
-            }
-            else
-            {
-                if (Actions.ContainsKey(AttackingHero))
-                {
-                    Actions[AttackingHero] = "Feinte";
-                }
-                else
-                    Actions.Add(AttackingHero, "Feinte");
-            }
+            DoAllActions("Feinte", AITurn());
+            //if (AllyHero.Skills[2].UseCounter == 0)
+            //{
+            //    System.Windows.MessageBox.Show("Vous avez utilisé toutes vos feintes");
+            //}
+            //else
+            //{
+            //    if (Actions.ContainsKey(AllyHero))
+            //    {
+            //        Actions[AllyHero] = "Feinte";
+            //    }
+            //    else
+            //        Actions.Add(AllyHero, "Feinte");
+            //}
         }
 
         private void ParadeButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            if (AttackingHero.Skills[2].UseCounter == 0)
+            if (AllyHero.Skills[1].UseCounter == 0)
             {
                 System.Windows.MessageBox.Show("Vous avez utilisé toutes vos parades");
             }
             else
             {
-                if (Actions.ContainsKey(AttackingHero))
-                {
-                    Actions[AttackingHero] = "Parade";
-                }
-                else
-                    Actions.Add(AttackingHero, "Parade");
+
+                DoAllActions("Parade", AITurn());
+                AllyHero.Skills[1].UseCounter--;
+                //if (Actions.ContainsKey(AllyHero))
+                //{
+                //    Actions[AllyHero] = "Parade";
+                //}
+                //else
+                //    Actions.Add(AllyHero, "Parade");
             }
         }
 
         private void AttaqueButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            if (Actions.ContainsKey(AttackingHero))
-            {
-                Actions[AttackingHero] = "Attaque";
-            }
-            else
-                Actions.Add(AttackingHero, "Attaque");
+            DoAllActions("Attaque", AITurn());
+            //if (Actions.ContainsKey(AllyHero))
+            //{
+            //    Actions[AllyHero] = "Attaque";
+            //}
+            //else
+            //    Actions.Add(AllyHero, "Attaque");
         }
 
         /// <summary>
@@ -179,51 +195,105 @@ namespace Clickers.ViewModel.Army
         /// 1°) Parade 2°) Feinte 3°) Attaque
         /// On remet ensuite toutes les parades à false
         /// </summary>
-        private void DoAllActions()
+        private void DoAllActions(String allyHeroAction, String enemyHeroAction)
         {
-            foreach (KeyValuePair<Hero, string> heroWithAction in Actions)
+            if (allyHeroAction == "Objet")
             {
-                if (heroWithAction.Value == "Parade")
+                if (this.AllyHero.Potion != null)
                 {
-                    heroWithAction.Key.IsParing = true;
+                    this.Heal(this.AllyHero);
                 }
             }
-            foreach (KeyValuePair<Hero, string> heroWithAction in Actions)
+            if (enemyHeroAction == "Objet")
             {
-                if (heroWithAction.Value == "Feinte")
-                {
-                    if (heroWithAction.Key == AttackingHero)
-                    {
-                        Feinte(AttackingHero, DefendingHero);
-                    }
-                    else
-                    {
-                        Feinte(DefendingHero, AttackingHero);
-                    }
-                }
+                this.Heal(this.EnemyHero);
             }
-            foreach (KeyValuePair<Hero, string> heroWithAction in Actions)
+            if (allyHeroAction == "Parade")
             {
-                if (heroWithAction.Value == "Attaque")
-                {
-                    if (heroWithAction.Key == AttackingHero)
-                    {
-                        Attack(AttackingHero, DefendingHero);
-                    }
-                    else
-                    {
-                        Attack(DefendingHero, AttackingHero);
-                    }
-                }
-                if (AttackingHero.Life <= 0 ||DefendingHero.Life <= 0)
+                this.AllyHero.IsParing = true;
+            }
+            if (enemyHeroAction == "Parade")
+            {
+                this.EnemyHero.IsParing = true;
+            }
+            if (allyHeroAction == "Feinte")
+            {
+                this.Feinte(this.AllyHero, this.EnemyHero);
+            }
+            if (enemyHeroAction == "Feinte")
+            {
+                this.Feinte(this.EnemyHero, this.AllyHero);
+            }
+            if (allyHeroAction == "Attaque")
+            {
+                this.Attack(this.AllyHero, this.EnemyHero);
+                if (this.AllyHero.Life <= 0 || this.EnemyHero.Life <= 0)
                 {
                     EndFight();
-                    break;
                 }
             }
-            this.AttackingHero.IsParing = false;
-            this.DefendingHero.IsParing = false;
+            if (enemyHeroAction == "Attaque")
+            {
+                this.Attack(this.EnemyHero, this.AllyHero);
+
+                if (this.AllyHero.Life <= 0 || this.EnemyHero.Life <= 0)
+                {
+                    EndFight();
+                }
+            }
+            //foreach (KeyValuePair<Hero, string> heroWithAction in Actions)
+            //{
+            //    if (heroWithAction.Value == "Parade")
+            //    {
+            //        heroWithAction.Key.IsParing = true;
+            //    }
+            //}
+            //foreach (KeyValuePair<Hero, string> heroWithAction in Actions)
+            //{
+            //    if (heroWithAction.Value == "Feinte")
+            //    {
+            //        if (heroWithAction.Key == AttackingHero)
+            //        {
+            //            Feinte(AttackingHero, DefendingHero);
+            //        }
+            //        else
+            //        {
+            //            Feinte(DefendingHero, AttackingHero);
+            //        }
+            //    }
+            //}
+            //foreach (KeyValuePair<Hero, string> heroWithAction in Actions)
+            //{
+            //    if (heroWithAction.Value == "Attaque")
+            //    {
+            //        if (heroWithAction.Key == AttackingHero)
+            //        {
+            //            Attack(AttackingHero, DefendingHero);
+            //        }
+            //        else
+            //        {
+            //            Attack(DefendingHero, AttackingHero);
+            //        }
+            //    }
+            //    if (AttackingHero.Life <= 0 ||DefendingHero.Life <= 0)
+            //    {
+            //        EndFight();
+            //        break;
+            //    }
+            //}
+            this.AllyHero.IsParing = false;
+            this.EnemyHero.IsParing = false;
             Turn++;
+        }
+
+        public void Heal(Hero hero)
+        {
+            hero.Life += hero.Potion.HealValue;
+            if (hero.Life > hero.BaseLife)
+            {
+                hero.Life = hero.BaseLife;
+            }
+            hero.Potion = null;
         }
 
         public void Feinte(Hero feintingHero, Hero ennemyHero)
@@ -259,8 +329,9 @@ namespace Clickers.ViewModel.Army
         /// <summary>
         /// Méthode gérant le tour de l'adversaire. (Intelligence artificielle)
         /// </summary>
-        private void AITurn()
+        private String AITurn()
         {
+            String actionToReturn = "";
             /*Génération d'un nombre au hasard entre 0 et 40 pour définir l'action à réaliser :
              * 0 à 10 Attaque
              * 11 à 20 Feinte
@@ -271,60 +342,73 @@ namespace Clickers.ViewModel.Army
             int actionNumber = rand.Next(0, 41);
             if (actionNumber <= 10)
             {
-                if (Actions.ContainsKey(DefendingHero))
-                {
-                    Actions[DefendingHero] = "Attaque";
-                }
-                else
-                    Actions.Add(DefendingHero, "Attaque");
+                actionToReturn = "Attaque";
+                //if (Actions.ContainsKey(EnemyHero))
+                //{
+                //    Actions[EnemyHero] = "Attaque";
+                //}
+                //else
+                //    Actions.Add(EnemyHero, "Attaque");
             }
             else if (actionNumber > 10 && actionNumber <= 20)
             {
-                if (DefendingHero.Skills[1].UseCounter == 0)
-                {
-                    AITurn();
-                }
-                else
-                {
-                    if (Actions.ContainsKey(DefendingHero))
-                    {
-                        Actions[DefendingHero] = "Feinte";
-                    }
-                    else
-                        Actions.Add(DefendingHero, "Feinte");
-                }
+                actionToReturn = "Feinte";
+                //if (Actions.ContainsKey(EnemyHero))
+                //{
+                //    Actions[EnemyHero] = "Feinte";
+                //}
+                //else
+                //    Actions.Add(EnemyHero, "Feinte");
             }
             else if (actionNumber > 20 && actionNumber <= 30)
             {
-                if (DefendingHero.Skills[2].UseCounter == 0)
+
+                if (EnemyHero.Skills[1].UseCounter == 0)
                 {
                     AITurn();
                 }
                 else
                 {
-                    if (Actions.ContainsKey(DefendingHero))
-                    {
-                        Actions[DefendingHero] = "Parade";
-                    }
-                    else
-                        Actions.Add(DefendingHero, "Parade");
+                    actionToReturn = "Parade";
+                    EnemyHero.Skills[1].UseCounter--;
                 }
+                //if (EnemyHero.Skills[2].UseCounter == 0)
+                //{
+                //    AITurn();
+                //}
+                //else
+                //{
+                //    if (Actions.ContainsKey(EnemyHero))
+                //    {
+                //        Actions[EnemyHero] = "Parade";
+                //    }
+                //    else
+                //        Actions.Add(EnemyHero, "Parade");
+                //}
             }
             else
             {
-                if (Actions.ContainsKey(DefendingHero))
+                if (this.EnemyHero.Potion != null)
                 {
-                    Actions[DefendingHero] = "Object";
+                    actionToReturn = "Potion";
                 }
                 else
-                    Actions.Add(DefendingHero, "Object");
+                    AITurn();
+                //if (Actions.ContainsKey(EnemyHero))
+                //{
+                //    Actions[EnemyHero] = "Object";
+                //}
+                //else
+                //    Actions.Add(EnemyHero, "Object");
             }
+
+            return actionToReturn;
         }
 
         private void EndFight()
         {
             MessageBoxResult msgBxResult;
-            if (DefendingHero.Life <= 0)
+            if (EnemyHero.Life <= 0)
             {
                 msgBxResult = System.Windows.MessageBox.Show("You win");
             }
@@ -341,7 +425,7 @@ namespace Clickers.ViewModel.Army
         private void launchBattle()
         {
             GameViewModel.Instance.EnnemyCastle.Army.GenerateEnnemyArmy();
-            Battle newBattle = new Battle(attackingArmy,defendingArmy,attackedCastle);
+            Battle newBattle = new Battle(attackingArmy, defendingArmy, attackedCastle);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
